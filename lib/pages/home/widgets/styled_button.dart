0a5655c08b1212a439ae1aeb1e8ea8c160cbd537
tarus <victortarus6@gmail.com>
2024-05-11 15:05:03 +0300

    added home page container

--- lib/pages/home/widgets/styled_button.dart
+++ lib/pages/home/widgets/styled_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-class StyledButton extends StatelessWidget{
-  const StyledButton(this.text,{
-    super.key
-  });
+class StyledButton extends StatelessWidget {
+  const StyledButton(this.text, {super.key});
   final String text;
-@override
-  Widget build(BuildContext context){
-return FilledButton(onPressed: onPressed, child: child)
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+        onPressed: onPressed,
+        style: FilledButton.styleFrom(
+            backgroundColor: Colors.brown, foregroundColor: Colors.white),
+        child: Text(text));
   }
 }
