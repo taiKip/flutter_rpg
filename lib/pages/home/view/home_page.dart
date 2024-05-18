57e90a429f957e30792626e84bea7ad3953edc0f
tarus <victortarus6@gmail.com>
2024-05-18 23:53:55 +0300

    updated home page

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -10,37 +10,3 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  List characters = ['mario', 'luigi', 'peach', 'toad', 'bowser', 'koopa'];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const StyledTitle('Your characters'),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: characters.length,
-                itemBuilder: (_, index) {
-                  return Container(
-                    color: Colors.grey[800],
-                    padding: const EdgeInsets.all(40),
-                    margin: const EdgeInsets.only(bottom: 40),
-                    child: Text(characters[index]),
-                  );
-                },
-              ),
-            ),
-            StyledButton(
-                onPressed: () {}, child: const StyledHeading("Create New"))
-          ],
-        ),
-      ),
-    );
-  }
-}
