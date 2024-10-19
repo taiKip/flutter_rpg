8cf25d5c7fa1fbff15a55855234e34f8527cfaf8
tarus <victortarus6@gmail.com>
2024-10-19 21:15:29 +0300

    added google fonts

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Characters'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const StyledText('Character List'),
+            const StyledHeading('Character List'),
+            const StyledTitle('Character List'),
+            FilledButton(onPressed: () {}, child: const Text('Create new'))
+          ],
+        ),
+      ),
+    );
+  }
+}
